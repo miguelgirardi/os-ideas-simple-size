0d3f711bf470bb9c49c3bfa64ff09034647a5213
Miguel dos Santos Girardi <miguel-girardi@estudante.rs.gov.br>
2026-06-12 14:49:22 -0300

    Implement echo command with special case
    Add echo command handling for specific message.

--- migos2k.py
+++ migos2k.py
@@ -4,4 +4,11 @@ def usarKernel():
     if cmd=="about":
       print("MigOS version 2KiB")
       print("Made in 2026")
+    elif cmd.startswith("echo"):
+      if cmd!="echo TESTMESSAGE:
+        print(cmd[5:])
+      else:
+        print("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+    
 usarKernel()
